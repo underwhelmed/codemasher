bb990793732c5819c72f8d10c246a40448f8722d
Stephen Wright <wright.net@gmail.com>
2010-10-27 08:50:40 -0500

    updated memcache declaration

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.gem 'memcached-northscale', :lib => 'memcached'
+require 'memcached'
+
 # Initialize the rails application
 Codemasher::Application.initialize!
