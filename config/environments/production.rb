e69feef85bcdb55e14f275f381cdb257f9e28a96
Stephen Wright <wright.net@gmail.com>
2010-10-27 14:43:21 -0500

    changed memcache client to dalli which supports Ruby 1.9.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Codemasher::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
+  config.cache_store = :dalli_store, ENV['MEMCACHE_SERVERS']
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
