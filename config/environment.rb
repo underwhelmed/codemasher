e6ec7a4bed1dda3a1e290c24e5b2df2b2be21e2c
Stephen Wright <wright.net@gmail.com>
2010-10-27 08:52:01 -0500

    fixed issue with environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.gem 'memcached-northscale', :lib => 'memcached'
-require 'memcached'
-
 # Initialize the rails application
 Codemasher::Application.initialize!
